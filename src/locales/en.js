d986b0cd81fe9350ad05bae2c7393d287989f1d6
Arkadiy Konstantinov <106591827+ArkadiyKonstantinov@users.noreply.github.com>
2023-03-07 16:24:26 +0300

    Complete step 3

---
+++ src/locales/en.js
@@ -0,0 +1,15 @@
+export default {
+  translation: {
+    text: {
+      header: 'RSS agregator',
+      slogan: 'Start reading RSS today! It\'s easy, it\'s beautiful.',
+      example: 'Example: https://ru.hexlet.io/lessons.rss',
+      label: 'RSS link',
+    },
+    errors: {
+      validate: {
+        not_valid_url: 'Link must be a valid URL',
+      },
+    },
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,15 @@
+export default {
+  translation: {
+    text: {
+      header: 'RSS агрегатор',
+      slogan: 'Начните читать RSS сегодня! Это легко, это красиво.',
+      example: 'Пример: https://ru.hexlet.io/lessons.rss',
+      label: 'Ссылка RSS',
+    },
+    errors: {
+      validate: {
+        not_valid_url: 'Ссылка должна быть валидным URL',
+      },
+    },
+  },
+};
