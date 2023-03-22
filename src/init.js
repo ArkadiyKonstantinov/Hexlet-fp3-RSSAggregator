aa722c061c23101962addec52d32f665138e911e
Arkadiy Konstantinov <106591827+ArkadiyKonstantinov@users.noreply.github.com>
2023-03-22 13:33:00 +0300

    Create init.js

---
+++ src/init.js
@@ -0,0 +1,111 @@
+import uniqueId from 'lodash/uniqueId.js';
+import i18next from 'i18next';
+import axios from 'axios';
+import * as yup from 'yup';
+import resources from './locales/index.js';
+import watch from './view.js';
+import parse from './parse.js';
+
+const proxifyUrl = (rssUrl) => {
+  const url = new URL('/get', 'https://allorigins.hexlet.app/');
+  url.searchParams.set('disableCache', true);
+  url.searchParams.set('url', rssUrl);
+  return url;
+};
+
+const validate = (state, url) => {
+  yup.setLocale({
+    string: {
+      url: () => ('Must be valid'),
+    },
+    mixed: {
+      required: () => ('Input is empty'),
+      notOneOf: () => ('Already exists'),
+    },
+  });
+
+  const schema = yup.string().url().required().notOneOf(state.feeds.map((f) => f.url));
+  return schema.validate(url);
+};
+
+const updateFeed = (feed, state) => {
+  const url = proxifyUrl(feed.url);
+  axios.get(url)
+    .then((response) => parse(response.data.contents))
+    .then((parsedData) => {
+      const newPosts = parsedData.items
+        .filter((item) => !state.posts.find((post) => post.title === item.title))
+        .map((item) => ({ ...item, feedId: feed.feedId, postId: uniqueId() }));
+      if (newPosts) {
+        state.posts.unshift(...newPosts);
+      }
+    })
+    .then(() => setTimeout(() => updateFeed(feed, state), 5000))
+    .catch((error) => { throw error; });
+};
+
+const app = (initialState, elements, i18n) => {
+  const watchedState = watch(initialState, elements, i18n);
+  watchedState.lng = i18n.lng;
+
+  elements.form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const url = elements.urlInput.value;
+    validate(watchedState, url)
+      .then(() => {
+        watchedState.form.processState = 'adding';
+      })
+      .then(() => axios.get(proxifyUrl(url)).then((response) => response.data.contents))
+      .then((contents) => {
+        const parsedData = parse(contents);
+        const feed = {
+          feedId: uniqueId(),
+          url,
+          title: parsedData.title,
+          description: parsedData.description,
+        };
+        const posts = parsedData.items.map((item) => ({
+          feedId: feed.feedId,
+          postId: uniqueId(),
+          ...item,
+        }));
+        watchedState.feeds.unshift(feed);
+        watchedState.posts.unshift(...posts);
+        watchedState.form.processFeedback = { key: 'feedback.success.feedAdded', type: 'success' };
+        watchedState.form.processState = 'filling';
+        return feed;
+      })
+      .then((feed) => updateFeed(feed, watchedState))
+      .catch((error) => {
+        const errorsMap = new Map([
+          ['Must be valid', { key: 'feedback.error.notValidURL', type: 'error' }],
+          ['Input is empty', { key: 'feedback.error.urlRequired', type: 'error' }],
+          ['Already exists', { key: 'feedback.error.alreadyExists', type: 'error' }],
+          ['Network Error', { key: 'feedback.error.netError', type: 'error' }],
+          ['Parse error!', { key: 'feedback.error.parsingError', type: 'error' }],
+        ]);
+        watchedState.form.processFeedback = errorsMap.get(error.message);
+        watchedState.form.processState = 'failed';
+        console.dir(error);
+      });
+  });
+
+  elements.postsContainer.addEventListener('click', (e) => {
+    const { id } = e.target.dataset;
+    if (!id) {
+      return;
+    }
+    watchedState.ui.modalPostId = id;
+    watchedState.ui.readPosts.push(id);
+  });
+};
+
+export default (state, elements) => {
+  const i18nInstance = i18next.createInstance();
+  i18nInstance
+    .init({
+      lng: 'ru',
+      resources,
+    })
+    .then(() => app(state, elements, i18nInstance));
+};
